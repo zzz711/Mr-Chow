7e090d1c78e4f4a4c4546603d3b3e6d72ef0b8e8
jamesa8 <jamesa8@mail.uc.edu>
2016-03-13 20:38:01 -0400

    CSS changes for Nicket

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -291,7 +291,15 @@ app.controller('addMedicineCtrl', function ($scope, medicineService, $state) {
     $scope.picture = "";
     $scope.height = "0px";
     $scope.width = "0px";
-
+    $scope.graph = {};
+    $scope.graph.data = [
+      //Awake
+      [16, 15, 20, 12, 16, 12, 8],
+      //Asleep
+      [8, 9, 4, 12, 8, 12, 14]
+    ];
+    $scope.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    $scope.graph.series = ['Awake', 'Asleep'];
     $scope.returnVals = "";
     $scope.totalVal = addIngredientService.getTotalContents($http);
 
